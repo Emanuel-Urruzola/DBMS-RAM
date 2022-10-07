bbc17352eef2d795c77fe40baed012fef926896c
Emanuel Urruzola <emaurruzola@gmail.com>
2022-10-07 18:47:49 -0300

    Include a .clang-format archive, and formatting code

--- tables.hpp
+++ tables.hpp
@@ -8,27 +8,27 @@
 
 using namespace std;
 
-void createTable(Tables &tablesList, string tableName) {
-  Tables newTable = new nodeTable;
-  newTable->name = tableName;
+void createTable( Tables &tablesList, string tableName ) {
+  Tables newTable      = new nodeTable;
+  newTable->name       = tableName;
   newTable->attributes = NULL;
-  newTable->tuple = NULL;
-  newTable->next = tablesList;
-  tablesList = newTable;
+  newTable->tuple      = NULL;
+  newTable->next       = tablesList;
+  tablesList           = newTable;
 }
 
-void showTables(Tables tablesList) {
+void showTables( Tables tablesList ) {
   Tables aux = tablesList;
-  while (aux != NULL) {
+  while( aux != NULL ) {
     cout << aux->name << endl;
     aux = aux->next;
   }
 }
 
-Tables findTable(Tables &tablesList, string tableName) {
+Tables findTable( Tables &tablesList, string tableName ) {
   Tables aux = tablesList;
-  while (aux != NULL) {
-    if (aux->name == tableName) return aux;
+  while( aux != NULL ) {
+    if( aux->name == tableName ) return aux;
     aux = aux->next;
   }
   return NULL;
--- tuples.hpp
+++ tuples.hpp
@@ -3,31 +3,31 @@
 #include "tables.hpp"
 #include "variables.hpp"
 
-bool validColumns(string columnsOrder, Tables table) {
+bool validColumns( string columnsOrder, Tables table ) {
   size_t position;
   int userAttributesCounter = 0;
   // TODO: Check possible double name in columns
   // TODO: Validate types and restrictions
-  while ((position = columnsOrder.find(":")) != string::npos) {
+  while( ( position = columnsOrder.find( ":" ) ) != string::npos ) {
     userAttributesCounter++;
-    int counter = 0;
+    int counter               = 0;
     Tuple tableAttributesCopy = table->attributes;
-    while (tableAttributesCopy != NULL) {
-      if (columnsOrder.substr(0, position) == tableAttributesCopy->name)
+    while( tableAttributesCopy != NULL ) {
+      if( columnsOrder.substr( 0, position ) == tableAttributesCopy->name )
         counter++;
       tableAttributesCopy = tableAttributesCopy->next;
     }
-    if (counter != 1) {
+    if( counter != 1 ) {
       return false;
     }
-    columnsOrder.erase(0, position + 1);
+    columnsOrder.erase( 0, position + 1 );
   }
-  if (userAttributesCounter == table->attributes->index) return true;
+  if( userAttributesCounter == table->attributes->index ) return true;
   return false;
 }
-void InsertInto(string tableName, string columnsOrder, string columnValues) {
-  Tables table = findTable(tablesList, tableName);
-  if (table == NULL) {
+void InsertInto( string tableName, string columnsOrder, string columnValues ) {
+  Tables table = findTable( tablesList, tableName );
+  if( table == NULL ) {
     cout << "Doesn't exist";
   } else {
     cout << "Exist";
