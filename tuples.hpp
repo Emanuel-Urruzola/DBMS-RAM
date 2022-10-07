50d5a23cea7fc5a8a4e6c1bdc528e3b2a29c1e1e
Steven <stevenacostauy@gmail.com>
2022-10-06 22:40:15 -0300

    Test commit

--- tuples.hpp
+++ tuples.hpp
@@ -2,33 +2,38 @@
 #define tuples
 #include "variables.hpp"
 #include "tables.hpp"
-bool validColumns(string columnsOrder, Tables table){
+bool validColumns(string columnsOrder, Tables table)
+{
   size_t position;
   int userAttributesCounter = 0;
-  //TODO: Check possible double name in columns
-  //TODO: Validate types and restrictions
-  while( ( position = columnsOrder.find(":") ) != string::npos){
+  // TODO: Check possible double name in columns
+  // TODO: Validate types and restrictions
+  while ((position = columnsOrder.find(":")) != string::npos)
+  {
     userAttributesCounter++;
     int counter = 0;
     Tuple tableAttributesCopy = table->attributes;
-    while ( tableAttributesCopy != NULL )
+    while (tableAttributesCopy != NULL)
     {
-      if (columnsOrder.substr( 0 , position ) == tableAttributesCopy->name)
+      if (columnsOrder.substr(0, position) == tableAttributesCopy->name)
         counter++;
       tableAttributesCopy = tableAttributesCopy->next;
     }
-    if ( counter != 1 ){
+    if (counter != 1)
+    {
       return false;
     }
-    columnsOrder.erase( 0 , position+1 );
+    columnsOrder.erase(0, position + 1);
   }
-  if ( userAttributesCounter == table->attributes->index)
+  if (userAttributesCounter == table->attributes->index)
     return true;
-  return false;  
+  return false;
 }
-void InsertInto(string tableName, string columnsOrder, string columnValues){
-  Tables table = findTable(tablesList,tableName);
-  if ( table == NULL ){
+void InsertInto(string tableName, string columnsOrder, string columnValues)
+{
+  Tables table = findTable(tablesList, tableName);
+  if (table == NULL)
+  {
     cout << "Doesn't exist";
   }
   else
@@ -36,4 +41,4 @@ void InsertInto(string tableName, string columnsOrder, string columnValues){
     cout << "Exist";
   }
 }
-#endif // !tuples
+#endif // !tuples test
