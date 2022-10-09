206ea5392bd4d79fb905c2a2925915b7592496af
Jonathan Netto <jonanetto1@gmail.com>
2022-10-09 17:05:35 -0300

    Fix the createTable function. Don't allow duplicated tables

--- tables.hpp
+++ tables.hpp
@@ -7,14 +7,40 @@
 #include "variables.hpp"
 
 using namespace std;
-
-void createTable( Tables tablesList, string tableName ) {
-  Tables newTable      = new nodeTable;
-  newTable->name       = tableName;
-  newTable->attributes = NULL;
-  newTable->tuple      = NULL;
-  newTable->next       = tablesList;
-  tablesList           = newTable;
+// Esta pasando que tira el error, pero sigue iterando y comparando, preciso que
+// no lo haga, si no me va a crear la tabla de igual manera
+void createTable( string tableName ) {
+  if( tablesList != NULL ) {
+    Tables aux  = tablesList;
+    bool finded = false;
+    cout<<"";
+    while( aux != NULL && ! finded ) {
+      if( aux->name == tableName ) {
+        cout << "Error" << endl;
+        finded = true;
+      } else /**/ {
+        if( aux->next != NULL ) {
+          aux = aux->next;
+        } else {
+          cout << "";
+          Tables newTable      = new nodeTable;
+          newTable->name       = tableName;
+          newTable->attributes = NULL;
+          newTable->tuple      = NULL;
+          newTable->next       = tablesList;
+          tablesList           = newTable;
+          aux                  = aux->next;
+        }
+      }
+    }
+  } else {
+    Tables newTable      = new nodeTable;
+    newTable->name       = tableName;
+    newTable->attributes = NULL;
+    newTable->tuple      = NULL;
+    newTable->next       = tablesList;
+    tablesList           = newTable;
+  }
 }
 
 void showTables( Tables tablesList ) {
