4fb9a34109034196c932ecf7bfe5f4971634e075
StevenAcosta24 <65461875+StevenAcosta24@users.noreply.github.com>
2022-10-20 14:34:44 -0300

    Update tuples.cpp

--- src/tuples.cpp
+++ src/tuples.cpp
@@ -16,6 +16,7 @@ void splitCondition( string condition, string &column, string &value,
 bool validColumns( string columnsOrder, Tables table ) {
   size_t position;
   int userAttributesCounter = 0;
+  // Falla al recortar, al ser una sola columna no tendra el ":"
   while( ( position = columnsOrder.find( ":" ) ) != string::npos ) {
     userAttributesCounter++;
     int counter               = 0;
@@ -30,6 +31,8 @@ bool validColumns( string columnsOrder, Tables table ) {
   }
   Tuple tableAttributesCopy = table->attributes;
   int attributesCounter     = 0;
+  if(tableAttributesCopy->next == NULL) attributesCounter++;
+  
   while( tableAttributesCopy->next != NULL ) {
     attributesCounter++;
     tableAttributesCopy = tableAttributesCopy->next;
