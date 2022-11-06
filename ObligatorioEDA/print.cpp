c3713bbcd07e5d61baa9dc59c93381dcec544148
Jonathan Netto <jonanetto1@gmail.com>
2022-11-06 00:02:19 -0300

    printMetadata changes

--- ObligatorioEDA/print.cpp
+++ ObligatorioEDA/print.cpp
@@ -36,20 +36,6 @@ string getRowString( Tuple row, int index, typeOfData& type, int& number,
   }
   return rowString;
 }
-string getRowString2( Tuple row, typeOfData& type, ListInt length ) {
-  string rowString = "|";
-  while( row != NULL ) {
-    string element = "";
-    if( row->type == typeOfData::STRING ) element = "STRING";
-    else
-      element = "INT";
-    element.resize( length->value, ' ' );
-    rowString += " " + element + " |";
-    row    = row->next;
-    length = length->next;
-  }
-  return rowString;
-}
 
 int maxlenght( Tuple row ) {
   int lengthRow = 2;
@@ -63,14 +49,6 @@ int maxlenght( Tuple row ) {
       row->type == typeOfData::STRING && row->text == "" ) {
     if( lengthRow < 5 ) lengthRow = 5;
   }
-  return lengthRow;
-}
-
-int maxNameLength( Tuple attributes ) {
-  int lengthRow = 2;
-  if( attributes->name.length( ) > lengthRow )
-    lengthRow = attributes->name.length( );
-
   return lengthRow;
 }
 
@@ -96,29 +74,7 @@ void getLength( Tuples rows, ListInt list ) {
     rows = rows->next;
   }
 }
-void getLength2( Tuple attributes, ListInt list ) {
-  while( attributes != NULL ) {
-    int maximum = maxNameLength( attributes );
-    attributes  = attributes->next;
-  }
-}
 
-void loopInAttributes( Tuple attributes, TreeInt& treeQuerie,
-                       TreeStr& treeQuerieStr, typeOfData& type ) {
-  int number;   // To sort by number
-  string text;  // To sort by text
-  ListInt lengths = getFirstLength( attributes->next );
-  getLength2( attributes, lengths );
-  while( attributes != NULL ) {
-    string rowString = getRowString2( attributes->next, type, lengths );
-    if( type == typeOfData::STRING ) {
-      insertText( treeQuerieStr, "STRING", rowString );
-    } else {
-      insertText( treeQuerieStr, "INTEGER", rowString );
-    }
-    attributes = attributes->next;
-  }
-}
 void loopInRows( Tuples rows, TreeInt& treeQuerie, TreeStr& treeQuerieStr,
                  int indexInParameter, typeOfData& type ) {
   int number;   // To sort by number
@@ -207,13 +163,11 @@ typeRet printMetadata( string tableName ) {
     cout << "ERROR: La tabla '" << tableName << "' no existe." << endl;
     return typeRet::ERROR;
   }
-  cout << "Tabla " << table->name << endl << endl;
+  cout << endl << "Tabla " << table->name << endl;
   if( table->attributes == NULL )
     cout << "Esquema vacio, '" << table->name << "' no posee columnas.";
   else {
-    Tuple tableAttributesCopy  = table->attributes;
-    Tuple tableAttributesCopy2 = table->attributes;
-    Tuple tableAttributesCopy3 = table->attributes;
+    Tuple tableAttributesCopy = table->attributes;
     while( tableAttributesCopy != NULL ) {
       string attribute = tableAttributesCopy->name;
       attribute.resize( 12, ' ' );
@@ -223,6 +177,7 @@ typeRet printMetadata( string tableName ) {
       tableAttributesCopy = tableAttributesCopy->next;
     }
     cout << endl;
+    Tuple tableAttributesCopy2 = table->attributes;
     while( tableAttributesCopy2 != NULL ) {
       if( tableAttributesCopy2->type == typeOfData::STRING ) {
         string type = "STRING";
@@ -235,11 +190,13 @@ typeRet printMetadata( string tableName ) {
         cout << " " << type << " "
              << "|";
       }
-        tableAttributesCopy2 = tableAttributesCopy2->next;
+      tableAttributesCopy2 = tableAttributesCopy2->next;
     }
     cout << endl;
+    Tuple tableAttributesCopy3 = table->attributes;
     while( tableAttributesCopy3 != NULL ) {
-      if( tableAttributesCopy3->restriction == typeOfRestriction::PRIMARY_KEY ) {
+      if( tableAttributesCopy3->restriction ==
+          typeOfRestriction::PRIMARY_KEY ) {
         string type = "PRIMARY KEY";
         type.resize( 12, ' ' );
         cout << " " << type << " "
@@ -262,28 +219,3 @@ typeRet printMetadata( string tableName ) {
   }
   return typeRet::OK;
 }
-/*if( tableAttributesCopy->type == typeOfData::STRING )
-        cout << " "
-             << "STRING"
-             << " "
-             << "|" << endl;
-      else
-        cout << " "
-             << "INT"
-             << " "
-             << "|" << endl;
-      if( tableAttributesCopy->restriction == typeOfRestriction::PRIMARY_KEY )
-        cout << " "
-             << "PRIMARY KEY"
-             << " "
-             << "|" << endl;
-      else if( tableAttributesCopy->restriction == typeOfRestriction::ANY )
-        cout << " "
-             << "NOT EMPTY"
-             << " "
-             << "|" << endl;
-      else
-        cout << " "
-             << "ANY"
-             << " "
-             << "|" << endl;*/
